e5e330981d54bcee44d79440a1a9571f5c36aed8
Evgeny Stepanov <evgenos@list.ru>
2020-07-06 16:36:37 +0300

    fixed links

--- js/swiper.js
+++ js/swiper.js
@@ -3,10 +3,10 @@ let swiper = new Swiper('.swiper-container', {
     crossFade: true
   },
   loop: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
